82558d4617b054b7071c6633414c4404488898d0
Alexsey <mazaev.alexsey@yandex.ru>
2017-07-14 15:03:28 +0400

    add base logic to details activity

--- app/src/main/java/com/example/admin1337/notes/ui/notesdetails/NoteDetailsActivity.java
+++ app/src/main/java/com/example/admin1337/notes/ui/notesdetails/NoteDetailsActivity.java
@@ -1,9 +1,12 @@
 package com.example.admin1337.notes.ui.notesdetails;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-
 import com.example.admin1337.notes.R;
 
 /**
@@ -11,6 +14,19 @@ import com.example.admin1337.notes.R;
  */
 
 public class NoteDetailsActivity extends AppCompatActivity {
+
+    public static String DETAILS_EXTRA = "note_details_extra";
+
+    public static Intent getIntent(Context context, String id) {
+        Intent intent = new Intent(context, NoteDetailsActivity.class);
+        intent.putExtra(DETAILS_EXTRA, id);
+
+        return intent;
+    }
+
+    public static void navigateToDetailsActivity(Activity activity, String id) {
+        ActivityCompat.startActivity(activity, NoteDetailsActivity.getIntent(activity, id), null);
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
